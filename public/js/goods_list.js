f8b9be26dcd2606eab2c96f804682f6f3cf32912
YCWang <xd8204102@gmail.com>
2021-08-16 18:16:16 +0800

    feat: hover 購物車icon叫出購物車區塊

--- public/js/goods_list.js
+++ public/js/goods_list.js
@@ -1,6 +1,7 @@
 jQuery(function() {
     wipe()
     showGoodsList()
+    cartHoverEvent()
 })
 
 async function showGoodsList() {
@@ -35,4 +36,26 @@ function getGoodsList() {
     var sendData = new Object()
 
     return jQuery.get(baseUrl + '/api/goods', sendData)
+}
+
+function cartHoverEvent() {
+    var miniCartElement = document.getElementById('mini-cart-block')
+    var miniCart = new bootstrap.Offcanvas(miniCartElement)
+
+    miniCartElement.addEventListener('shown.bs.offcanvas', function () {
+        //console.log('showing')
+        cartShowingFlag = true
+    })
+
+    miniCartElement.addEventListener('hide.bs.offcanvas', function () {
+        //console.log('hiding')
+        cartShowingFlag = false
+    })
+
+    var cartShowingFlag = false
+    jQuery("#mini-cart-icon").hover(function() {
+        if (cartShowingFlag == false) {
+            miniCart.show()
+        }
+    })
 }
--- resources/views/goods_list.blade.php
+++ resources/views/goods_list.blade.php
@@ -64,7 +64,7 @@
                                     </div>
                                 </li>
                             @endguest
-                                <li class="nav-item">
+                                <li class="nav-item" id="mini-cart-icon">
                                     <span><i class="fas fa-shopping-cart"></i></span>
                                     <!-- style="display: flex; align-items: center;" &nbsp; -->
                                 </li>
@@ -113,6 +113,16 @@
                 </div>
             </div>
             <div class="col-1">
+            </div>
+        </div>
+
+        <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="mini-cart-block" aria-controls="offcanvasRight" aria-labelledby="mini-cart-blockLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="mini-cart-blockLabel">What you want</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <p>Try scrolling the rest of the page to see this option in action.</p>
             </div>
         </div>
     </body>
