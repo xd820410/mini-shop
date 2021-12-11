6d3452c60190e53219ba8b7655b2707f215f14da
Y.C.Wang <xd8204102@gmail.com>
2021-12-12 00:35:22 +0800

    fix: column end to end_at & unit test sample (You can't run test command without unittest sample file.)

--- database/seeders/DiscountTestSeeder.php
+++ database/seeders/DiscountTestSeeder.php
@@ -21,7 +21,7 @@ class DiscountTestSeeder extends Seeder
             'title' => '測試兩件八折',
             'payload' => '{"affected": [9527], "threshold": 2, "discount_type": "percent", "discount_value": 20}',
             'start_at' => '2021-12-05 11:17:36',
-            'end' => '2024-12-05 11:17:36',
+            'end_at' => '2024-12-05 11:17:36',
         ];
         $discountService->create($data);
     }
