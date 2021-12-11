f5b392b255c3b68941550aca7ed99d9a70444500
Y.C.Wang <xd8204102@gmail.com>
2021-12-12 04:04:44 +0800

    fix: payload json

--- database/seeders/DiscountTestSeeder.php
+++ database/seeders/DiscountTestSeeder.php
@@ -16,10 +16,16 @@ class DiscountTestSeeder extends Seeder
     public function run(DiscountService $discountService)
     {
         $discountService->truncate();
+        $payload = [
+            'affected' => [9527],
+            'threshold' => 2,
+            'discount_type' => 'percent',
+            'discount_value' => 20,
+        ];
         $data = [
             'type' => 1,
             'title' => '測試兩件八折',
-            'payload' => '{"affected": [9527], "threshold": 2, "discount_type": "percent", "discount_value": 20}',
+            'payload' => $payload,
             'start_at' => '2021-12-05 11:17:36',
             'end_at' => '2024-12-05 11:17:36',
         ];
