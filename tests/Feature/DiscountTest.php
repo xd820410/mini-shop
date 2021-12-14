43ad9e5e5ead1dbac5e01870a534c6d360ab6f72
Y.C.Wang <xd8204102@gmail.com>
2021-12-14 09:33:55 +0800

    test: In/not-in time interval of discount, we'll get correct discount or not?

--- tests/Feature/DiscountTest.php
+++ tests/Feature/DiscountTest.php
@@ -6,6 +6,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Artisan;
+use Mockery\MockInterface;
+use App\Services\CartService;
+use Carbon\Carbon;
+use App\Repositories\GoodsRepository;
 
 class DiscountTest extends TestCase
 {
@@ -21,14 +25,78 @@ class DiscountTest extends TestCase
     }
 
     /**
-     * A basic feature test example.
-     *
+     * 檢查優惠區間是否生效
+     * @dataProvider cartForTimeIntervalOfDiscount
      * @return void
      */
-    public function test_example()
+    public function test_timeInterval($rawCart, $goodsData, $now, $expectedTotal)
     {
-        $response = $this->get('/');
+        $this->partialMock(CartService::class, function (MockInterface $mock) use ($rawCart) {
+            $mock->shouldReceive('getSessionCart')
+                ->once()
+                ->andReturn($rawCart);
+        });
 
-        $response->assertStatus(200);
+        $this->partialMock(GoodsRepository::class, function (MockInterface $mock) use ($goodsData) {
+            $mock->shouldReceive('getByIds')
+                ->once()
+                ->with([9527])
+                ->andReturn(collect($goodsData));
+        });
+
+        Carbon::setTestNow($now);
+        //↓It works.
+        //dump('now: ', Carbon::now());
+        $response = $this->get('/cart');
+        $response->assertJson([
+            'total' => $expectedTotal
+        ], true);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function cartForTimeIntervalOfDiscount()
+    {
+        return [
+            [
+                [
+                    'goods_9527' => [
+                        'goods_id' => "9527",
+                        'quantity' => 3,
+                    ]
+                ],
+                [
+                    [
+                        'id' => 9527,
+                        'title' => 'Goods for test',
+                        'description' => null,
+                        'price' => 100,
+                        'image_path' => null
+                    ]
+                ],
+                Carbon::now(),
+                260
+            ],
+            [
+                [
+                    'goods_9527' => [
+                        'goods_id' => "9527",
+                        'quantity' => 3,
+                    ]
+                ],
+                [
+                    [
+                        'id' => 9527,
+                        'title' => 'Goods for test',
+                        'description' => null,
+                        'price' => 100,
+                        'image_path' => null
+                    ]
+                ],
+                new Carbon('2030-01-23 11:53:20'),
+                300
+            ]
+        ];
     }
 }
